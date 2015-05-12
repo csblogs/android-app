07fc5f9e1d89d044dd70eb3e3a5a0a2569a5101f
Alex Pringle <alex@alexpringle.co.uk>
2015-05-12 13:03:58 +0100

    add CSBlogsApp application class

---
+++ CS Blogs/src/main/java/com/csblogs/csblogsandroid/CSBlogsApp.java
@@ -0,0 +1,12 @@
+package com.csblogs.csblogsandroid;
+
+import android.app.Application;
+
+public class CSBlogsApp extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
